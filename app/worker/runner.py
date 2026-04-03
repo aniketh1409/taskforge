2c5b47147002cac1686eaea4f095f1c7f6900be3
aniketh1409 <anikeths@ualberta.ca>
2026-04-03 01:21:54 -0600

    worker job processing

--- app/worker/runner.py
+++ app/worker/runner.py
@@ -1,5 +1,5 @@
 from app.db.session import SessionLocal
-from app.core.constants import JOB_STATUS_RUNNING
+from app.core.constants import JOB_STATUS_RUNNING, JOB_STATUS_FAILED, JOB_STATUS_SUCCEEDED
 from app.db.models import Job
 
 
@@ -12,5 +12,20 @@ def process_job(job_id: str) -> None:
         job.status = JOB_STATUS_RUNNING
         db.commit()
         db.refresh(job)
+
+        try:
+            job.result = {
+                "Message" : "Working fine!",
+                "Author" : "Aniketh"
+            }
+            job.error = None
+            job.status = JOB_STATUS_SUCCEEDED
+            db.commit()
+
+        except Exception as e:
+            job.status = JOB_STATUS_FAILED
+            job.error = str(e)
+            db.commit()
+        
     finally:
         db.close()
